52197cf062443c04f2109b16ac82aff1e10a7b61
Sandeep-AnilKumar <sandeepkanil@gmail.com>
2016-09-11 19:02:49 -0500

    MSApriori Algo

--- src/pkotha6_sanilk2_CS583_Project1/GetFrequentItemSets.java
+++ src/pkotha6_sanilk2_CS583_Project1/GetFrequentItemSets.java
@@ -7,14 +7,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class GetFrequentItemSets {
-    public  static TreeSet<SingleItem> singleItemSet = new TreeSet<>(new MISComparator());
+    public static Set<SingleItem> singleItemSet = new TreeSet<>(new MISComparator());
+    public static Set<SingleItem> F1 = new TreeSet<>(new IDComparator());
+    public static List<ItemSet> Fk = new LinkedList<>();
+    public static int noOfTransactions = 0;
+    public static Map<Integer, SingleItem> singleItemMap = new HashMap<>();
 
     public static void readInput() throws IOException {
         BufferedReader  br = new BufferedReader(new FileReader("input-data.txt"));
@@ -23,7 +28,6 @@ public class GetFrequentItemSets {
         // building transactions add singleItems
         List<HashSet<Integer>> transactions = new ArrayList<HashSet<Integer>>();
         Map<Integer, Integer> singleItemCount = new TreeMap<>();
-        int noOfTransactions = 0;
         while ((currentLine = br.readLine()) != null) {
             noOfTransactions++;
             System.out.println(currentLine);
@@ -63,6 +67,9 @@ public class GetFrequentItemSets {
             currSingleItem.setMIS(itemMisMap.get(itemId));
             currSingleItem.setSupport(count/noOfTransactions);
             singleItemSet.add(currSingleItem);
+            if(!singleItemMap.containsKey(itemId)) {
+                singleItemMap.put(itemId, currSingleItem);
+            }
         }
 
         // printing the built datastructure
@@ -115,82 +122,111 @@ public class GetFrequentItemSets {
                 }
             }
         }
-        
-        System.out.println("prininting L");
+
         for (SingleItem item : singleItemSet) {
             System.out.println(item);
         }
     }
 
+    public static void generateF1() {
+        for(SingleItem item : singleItemSet) {
+            if(item.getSupport() > item.getMIS()) {
+                F1.add(item);
+            }
+        }
+        for(SingleItem item : F1) {
+            System.out.println(item);
+        }
+    }
 
-    
-    public static TreeSet<SingleItem> generateF1() {
-	TreeSet<SingleItem> F1 = new TreeSet<SingleItem>(new IDComparator());
-	for(SingleItem item : singleItemSet) {
-	    if(item.getSupport() > item.getMIS()) {
-		F1.add(item);
-	    }
-	}
-	
-	for(SingleItem item : F1) {
-	    System.out.println(item);
-	}
-	return F1;
+    public static void AprioriAlgorithm() {
+        int k = 2;
+        List<ItemSet> Ck = new ArrayList<>();
+        int[] curItems = new int[]{};
+        while(k == 2 || Fk != null) {
+            if(k == 2) {
+                generateLevel2Candidate(0.1);
+            }
+            else {
+                //generateMSCandidate();
+            }
+
+            Fk = new LinkedList<>();
+            for(ItemSet i : Ck) {
+                i.setCount(Transactions.getItemSetCount(i));
+            }
+
+            for(ItemSet i : Ck) {
+                curItems = new int[i.getItemsSet().size()];
+                int index = 0;
+                for(int item : i.getItemsSet()) {
+                    curItems[index++] = item;
+                }
+
+                if(i.getCount() >= singleItemMap.get(curItems[0]).getMIS()) {
+                    Fk.add(i);
+                }
+            }
+        }
+        //Union;
+        return;
     }
-    
+
     public static void generateLevel2Candidate(double p) {
-	ArrayList<ItemSet> c2 = new ArrayList<>();
-	ArrayList<SingleItem> listL = new ArrayList<SingleItem>(singleItemSet);
-	for(int index =0 ; index < listL.size(); index++) {
-	    SingleItem currItem = listL.get(index);
-	    if(currItem.getSupport() >= currItem.getMIS()){
-		for(int h = index+1; h < listL.size(); h++) {
-		    SingleItem secondItem = listL.get(h);
-		    if ( (secondItem.getSupport() >= secondItem.getMIS()) &&
-			 (Math.abs(currItem.getSupport() - secondItem.getSupport())<=p)
-		        ) {
-			ItemSet currItemSet = new ItemSet();
-			currItemSet.add(currItem.getItemID());
-			currItemSet.add(secondItem.getItemID());
-			c2.add(currItemSet);
-		    }
-		}
-	    }
-	}
-	
-	for(ItemSet itemSet : c2) {
-	    
-		System.out.println(itemSet.getItemsSet());
-	    
-	}
-	
+        ArrayList<ItemSet> c2 = new ArrayList<>();
+        ArrayList<SingleItem> listL = new ArrayList<SingleItem>(singleItemSet);
+        for(int index =0 ; index < listL.size(); index++) {
+            SingleItem currItem = listL.get(index);
+            if(currItem.getSupport() >= currItem.getMIS()){
+                for(int h = index+1; h < listL.size(); h++) {
+                    SingleItem secondItem = listL.get(h);
+                    if ( (secondItem.getSupport() >= secondItem.getMIS()) &&
+                            (Math.abs(currItem.getSupport() - secondItem.getSupport())<=p)
+                            ) {
+                        ItemSet currItemSet = new ItemSet();
+                        currItemSet.add(currItem.getItemID());
+                        currItemSet.add(secondItem.getItemID());
+                        c2.add(currItemSet);
+                    }
+                }
+            }
+        }
+
+        for(ItemSet itemSet : c2) {
+
+            System.out.println(itemSet.getItemsSet());
+
+        }
+
     }
-    
+
     public static void main(String[] args) throws IOException {
-        readInput();       
+        readInput();  
+        System.out.println("prininting L");
         generateL();
         System.out.println("generating F1");
         generateF1();
         System.out.println("prining c2");
         generateLevel2Candidate(0.1);
-	TestSuites();
+        System.out.println("Implementing Algorithm");
+        AprioriAlgorithm();
+        TestSuites();
     }
-    
+
     public static void TestSuites() throws IOException {
-	ItemSet i = new ItemSet();
-	i.add(20);
-	i.add(30);
-	i.add(40);
-	System.out.println(Transactions.getItemSetCount(i)==0);
-	//0 because it is not present.
+        ItemSet i = new ItemSet();
+        i.add(20);
+        i.add(30);
+        i.add(40);
+        System.out.println(Transactions.getItemSetCount(i)==0);
+        //0 because it is not present.
 
-	i = new ItemSet();
-	i.add(20);
-	i.add(30);
-	i.add(50);
+        i = new ItemSet();
+        i.add(20);
+        i.add(30);
+        i.add(50);
 
-	System.out.println(Transactions.getItemSetCount(i)==2);
-	//2 because it is present in first and last.
+        System.out.println(Transactions.getItemSetCount(i)==2);
+        //2 because it is present in first and last.
     }
-
 }
