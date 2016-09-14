76158d32a01734043c18cba9b510e5eec6ae4aa6
Sandeep-AnilKumar <sandeepkanil@gmail.com>
2016-09-14 17:47:40 -0500

    changed comaprator definition

--- src/pkotha6_sanilk2_CS583_Project1/SingleItem.java
+++ src/pkotha6_sanilk2_CS583_Project1/SingleItem.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 class MISComparator implements Comparator<SingleItem> {
     @Override
     public int compare(SingleItem o1, SingleItem o2) {
-        return (o1.getMIS() > o2.getMIS() ? 1 :  -1 );
+        return (o1.getMIS() > o2.getMIS() ? 1 : (o1.getMIS() < o2.getMIS() ? -1 : 
+            ((o1.getItemID() > o2.getItemID() ? 1 : (o1.getItemID() < o2.getItemID() ? -1 : 0)))) );
     }
 }
 
